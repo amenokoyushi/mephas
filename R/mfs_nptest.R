9a430386227760c3f790d94d5e6fa5efb7ff9db2
yi-apr <zhouyi0426@hotmail.com>
2018-04-16 14:19:21 +0900

    update

--- R/mfs_nptest.R
+++ R/mfs_nptest.R
@@ -2,7 +2,7 @@
 ##'
 ##' This is the details of this function
 ##' @title Non-paramatric tests
-##' @return The shiny web page of non-parametric statistical test 
+##' @return The shiny web page of non-parametric statistical test
 ##' @import shiny
 ##' @import ggplot2
 ##' @importFrom gridExtra grid.arrange
@@ -11,27 +11,13 @@
 ##' @importFrom DescTools SignTest
 ##' @importFrom RVAideMemoire mood.medtest
 
-##' @examples 
+##' @examples
 ##' mfs_nptest()
-##' @export 
+##' @export
 mfs_nptest <- function(){
-
-  #setwd("/Users/Yi/Shiny/shiny/ttest/")
-  #if (!require(shiny)) {install.packages("shiny")}; library(shiny)
-  #if (!require(shinythemes)) {install.packages("shinythemes")}; library(shinythemes)
-  #if (!require(ggplot2)) {install.packages("ggplot2")}; library(ggplot2)
-  #if (!require(ggthemes)) {install.packages("ggthemes")}; library(ggthemes)
-  #if (!require(Hmisc)) {install.packages("Hmisc")}; library(Hmisc)
-  #if (!require(gridExtra)) {install.packages("gridExtra")}; library(gridExtra)
-  #if (!require(reshape)) {install.packages("reshape")}; library(reshape)
-  #if (!require(DT)) {install.packages("DT")}; library(DT)
-  #if (!require(DescTools)) {install.packages("DescTools")}; library(DescTools)
-  #if (!require(RVAideMemoire)) {install.packages("RVAideMemoire")}; library(RVAideMemoire)
-  #if (!require(pastecs)) {install.packages("pastecs")}; library(pastecs)
-
 
 source("R/nptest_ui.R", local=TRUE)
 source("R/nptest_sv.R", local=TRUE)
 
-shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server, options = list("launch.browser"))
 }
